4a871a7bdc9737e9c6307d96a719dc29850f787c
Thales Fernandes <t.fernandes.proppg@proppg.butantan.gov.br>
2026-04-28 15:58:42 +0200

    Update database.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,26 +1,36 @@
 
 from datasets import load_dataset
-# Download
-#!wget https://github.com/crux82/squad-it/raw/master/SQuAD_it-train.json.gz
-#!wget https://github.com/crux82/squad-it/raw/master/SQuAD_it-test.json.gz
 #!wget "https://archive.ics.uci.edu/ml/machine-learning-databases/00462/drugsCom_raw.zip"
 #!unzip drugsCom_raw.zip
 
-# Load the local dataset
-data_files = {"train": "SQuAD_it-train.json.gz", "test": "SQuAD_it-test.json.gz"}
-squad_it_dataset = load_dataset("json", data_files=data_files, field="data")
-print(f"\nSQuAD_it dataset:\n {squad_it_dataset}")
-
-# Load a remote dataset
-url = "https://github.com/crux82/squad-it/raw/master/"
-data_files = {
-    "train": url + "SQuAD_it-train.json.gz",
-    "test": url + "SQuAD_it-test.json.gz",
-}
-squad_it_dataset = load_dataset("json", data_files=data_files, field="data")
-
-# Splitting
+# Load dataset
 data_files = {"train": "drugsComTrain_raw.tsv", "test": "drugsComTest_raw.tsv"}
 drug_dataset = load_dataset("csv", data_files=data_files, delimiter="\t")
-drug_sample = drug_dataset["train"].shuffle(seed=42).select(range(1000)) 
-print(f"\nDrug Review Dataset examples:\n {drug_sample[:3]}")
+
+# Data postprocessing
+def lowercase_condition(example):
+    return {"condition": example["condition"].lower()}
+
+
+def compute_review_length(example):
+    return {"review_length": len(example["review"].split())}
+
+drug_dataset = drug_dataset.filter(lambda x: x["condition"] is not None) # Drop empty rows
+
+drug_dataset = drug_dataset.rename_column(original_column_name="Unnamed: 0", new_column_name="patient_id") # Rename column
+
+drug_dataset.map(lowercase_condition) # Normalize
+
+drug_dataset = drug_dataset.map(compute_review_length) # Compute the length of the reviews
+
+drug_dataset = drug_dataset.filter(lambda x: x["review_length"] > 30) # Filter for reviewers with more than 30 words
+
+drug_dataset = drug_dataset.map(lambda x: {"review": html.unescape(x["review"])}) # Drop rows with HTML characters
+
+# Splitting
+drug_dataset_clean = drug_dataset["train"].train_test_split(train_size=0.8, seed=42)
+drug_dataset_clean["validation"] = drug_dataset_clean.pop("test")
+drug_dataset_clean["test"] = drug_dataset["test"]
+
+for split, dataset in drug_dataset_clean.items():
+    dataset.to_json(f"drug-reviews-{split}.jsonl")
